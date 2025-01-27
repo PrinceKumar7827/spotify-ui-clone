bbf79f986ed6ad8c2f22a786ca67aead77074c52
PrinceKumar7827 <princekumar7827312066@gmail.com>
2025-01-27 22:24:27 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+
+function toggler() {
+    // let a = document.getElementById('input-field');
+    let dispCont = document.getElementById('disp');
+    window.location.href = '#';
+    dispCont.style.display = 'none';
+    let disp2 = document.getElementById('disp2');
+    disp2.style.display = 'block';
+}
