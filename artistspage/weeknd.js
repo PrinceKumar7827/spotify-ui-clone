d7f5e490148f86afd544af806b5c602ad0b43830
PrinceKumar7827 <princekumar7827312066@gmail.com>
2025-01-27 22:22:49 +0530

    Add files via upload

---
+++ artistspage/weeknd.js
@@ -0,0 +1,20 @@
+function tog() {
+    let a = document.getElementById('last-footer1');
+    let b = document.getElementById('last-footer2');
+
+    a.style.display = 'none';
+    b.style.display = 'block';
+    b.style.display = 'flex';
+}
+
+let d = document.getElementById('st');
+// let e = d.value;
+
+console.log(d);
+// function tog2() {
+//     let a = document.getElementById('last-footer1');
+//     let b = document.getElementById('last-footer2');
+
+//     a.style.display = 'block';
+//     b.style.display = 'none';
+// }
